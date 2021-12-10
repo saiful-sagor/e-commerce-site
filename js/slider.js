3bd6f2795d3048f89a8beb919a70b7786390b9e7
saiful-sagor <sagor155670@gmail.com>
2021-12-10 16:48:57 +0600

    Latest Product added by Sagor

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,7 @@ Glide Js Carousel
 =============
  */
 const slider1 = document.getElementById("glide_1");
+const slider2 = document.getElementById("glide_2");
 
 
 
@@ -16,7 +17,7 @@ if(slider1){
   new Glide(slider1,{
     type: 'carousel',
     startAt:0,
-    // autoplay: 3000,
+    autoplay: 3000,
     hoverpause:true,
     perView:1,
     animationDuration:800,
@@ -25,12 +26,32 @@ if(slider1){
   }).mount();
 }
 
+
 /*
 =============
 Latest Products
 =============
  */
+if(slider2){
+  new Glide(slider2,{
+    type: 'carousel',
+    startAt:0,
+  
+    hoverpause:true,
+    perView:4,
+    animationDuration:800,
+    animationTimingFunc:"ease-in-out",
+    breakpoints:{
+      1200:{
+        perView:3
+      },
+      768:{
+        perView:2
+      },
+    }
 
+  }).mount();
+}
 
 /*
 =============
